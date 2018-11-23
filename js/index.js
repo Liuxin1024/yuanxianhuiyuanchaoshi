ddbe786f636d0e8fca441f633ac540a3b77bcd92
liuxin <747394004@qq.com>
2018-11-23 18:47:57 +0800

    master

--- js/index.js
+++ js/index.js
@@ -2,4 +2,20 @@ window.onload = function () {
   new Swiper ('.swiper-container', {
     loop: true, // 循环模式选项
   })
+
+
 }
+/*
+//得到时间并写入div
+function getDate(){
+  //获取当前时间
+  var date = new Date();
+  //格式化为本地时间格式
+  var date1 = date.toLocaleString();
+  //获取div
+  var div1 = document.getElementById("date");
+  //将时间写入div
+  div1.innerHTML = date1;
+}
+//使用定时器每秒向div写入当前时间
+setInterval("getDate()",1000);*/
